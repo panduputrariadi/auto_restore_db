6053c57f258c7a6d348866b68739a76fd3eb81ec
panduputragit <put.rariadi1144@gmail.com>
2024-04-24 16:23:42 +0800

    import databse with cli, fix syntax

--- cli-service/main.go
+++ cli-service/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"archive/zip"
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -10,134 +11,199 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
-
 func main() {
-	
-	zipFile := "../uploads/mysql_2024-04-24_5007b2ab-13fc-4be1-b2d9-74d1da73ea75.zip"
-    destDir := "../unzip"
-
-    if err := unzipExtractor(zipFile, destDir); err != nil {
-        fmt.Println("Error unzipping file:", err)
-    } else {
-        fmt.Println("Unzipping completed successfully")
-    }
-
-	configData, err := ioutil.ReadFile("../config.json")
-	if err != nil {
-		fmt.Printf("Error reading config file: %s\n", err)
-		return
-	}
-
-	var config []map[string]string
-	err = json.Unmarshal(configData, &config)
-	if err != nil {
-		fmt.Printf("Error decoding config JSON: %s\n", err)
-		return
-	}
-
-    dbHost := config[0]["db_host"]
-	dbPort := config[0]["db_port"]
-	dbName := config[0]["database_name"]
-	dbUser := config[0]["db_username"]
-	dbPassword := config[0]["db_password"]
-    filePath := "../unzip"
-
-    cmdString := fmt.Sprintf("mysql -u %s -p%s -h %s -P %s %s < %s", dbUser, dbPassword, dbHost, dbPort, dbName, filePath)
-
-    cmd := exec.Command("bash", "-c", cmdString)
-
-    // Jalankan perintah dan tangani error
-    err = cmd.Run()
-    if err != nil {
-        fmt.Printf("Error executing command: %s\n", err)
-        return
-    }
-
-    fmt.Println("Database imported successfully")
 
 	// URL layanan web, ganti {id} dengan ID yang sesuai.
-    fileURL := "http://localhost:3000/3/download"
+	fileURL := "http://localhost:3000/company/3/download"
 
-    // Path lokal tempat file akan disimpan
-    savePath := "../unzip/"
+	// Path lokal tempat file akan disimpan
+	saveDir := "../download/"
 
-    // Panggil fungsi download
-    if err := downloadFile(fileURL, savePath); err != nil {
-        fmt.Printf("Error downloading file: %s\n", err)
-    } else {
-        fmt.Println("File downloaded successfully.")
-    }
+	// Panggil fungsi download
+	if err := downloadFile(fileURL, saveDir); err != nil {
+		fmt.Printf("Error downloading file: %s\n", err)
+		return
+	} else {
+		fmt.Println("File downloaded successfully.")
+	}
 
+	dirPath := "../download/"
+	dir, err := os.Open(dirPath)
+	if err != nil {
+		fmt.Printf("Error opening directory: %s\n", err)
+		return
+	}
+	defer dir.Close()
+
+	files, err := dir.ReadDir(0)
+	if err != nil {
+		fmt.Printf("Error reading directory: %s\n", err)
+		return
+	}
+
+	for _, file := range files {
+		if !file.IsDir() {
+			zipFile := filepath.Join(dirPath, file.Name())
+			destDir := "../unzip/"
+
+			if err := unzipExtractor(zipFile, destDir); err != nil {
+				fmt.Println("Error unzipping file:", err)
+				return
+			} else {
+				fmt.Println("Unzipping completed successfully")
+			}
+
+			configData, err := ioutil.ReadFile("../config.json")
+			if err != nil {
+				fmt.Printf("Error reading config file: %s\n", err)
+				return
+			}
+
+			var config []map[string]string
+			err = json.Unmarshal(configData, &config)
+			if err != nil {
+				fmt.Printf("Error decoding config JSON: %s\n", err)
+				return
+			}
+
+			dbHost := config[0]["db_host"]
+			dbPort := config[0]["db_port"]
+			dbName := config[0]["database_name"]
+			dbUser := config[0]["db_username"]
+			// dbPassword := config[0]["db_password"]
+			
+
+			fileList, err := ioutil.ReadDir(destDir)
+			if err != nil {
+				fmt.Printf("Error reading directory: %s\n", err)
+				return
+			}
+
+			var sqlFile string
+			for _, f := range fileList {
+				if !f.IsDir() && strings.HasSuffix(f.Name(), ".sql") {
+					sqlFile = filepath.Join(destDir, f.Name())
+					break
+				}
+			}
+
+			if sqlFile == "" {
+				fmt.Println("No SQL file found in the destination directory")
+				return
+			}
+
+			
+			importCmd := fmt.Sprintf("mysql -u %s  -h %s -P %s %s < %s", dbUser, dbHost, dbPort, dbName, sqlFile)
+
+			var stdErr bytes.Buffer
+			cmd := exec.Command("bash", "-c", importCmd)
+
+			cmd.Stderr = &stdErr
+			err = cmd.Run()
+			if err != nil {
+				fmt.Printf("Error executing command: %s\n", stdErr.String())
+				return
+			}
+
+			fmt.Println("Database imported successfully")
+
+			if err := removeFiles(dirPath); err != nil {
+				fmt.Printf("Error removing files: %s\n", err)
+				return
+			}
+			if err := removeFiles(destDir); err != nil {
+				fmt.Printf("Error removing files: %s\n", err)
+				return
+			}
+		}
+	}
 }
 
-// unzipExtraktor akan mengekstrak file ZIP ke direktori tujuan.
 func unzipExtractor(zipFile, destDir string) error {
-    // Buka file ZIP untuk dibaca.
-    r, err := zip.OpenReader(zipFile)
-    if err != nil {
-        return err
-    }
-    defer r.Close()
+	r, err := zip.OpenReader(zipFile)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
 
-    os.MkdirAll(destDir, 0755)
-    for _, f := range r.File {
-        // Tentukan path file tujuan.
-        filePath := filepath.Join(destDir, f.Name)
+	os.MkdirAll(destDir, 0755)
+	for _, f := range r.File {
+		if filepath.Base(f.Name) == "__MACOSX" || strings.HasPrefix(filepath.Base(f.Name), "._") {
+			continue
+		}
+		filePath := filepath.Join(destDir, f.Name)
 
-        // Buat direktori untuk file jika diperlukan.
-        if f.FileInfo().IsDir() {
-            os.MkdirAll(filePath, f.Mode())
-            continue
-        }
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(filePath, f.Mode())
+			continue
+		}
 
-        // Buka file dalam arsip ZIP.
-        rc, err := f.Open()
-        if err != nil {
-            return err
-        }
-        defer rc.Close()
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer rc.Close()
 
-        // Buat file tujuan.
-        fDest, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-        if err != nil {
-            return err
-        }
-        defer fDest.Close()
+		fDest, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return err
+		}
+		defer fDest.Close()
 
-        // Salin isi file dari ZIP ke file tujuan.
-        _, err = io.Copy(fDest, rc)
-        if err != nil {
-            return err
-        }
-    }
+		_, err = io.Copy(fDest, rc)
+		if err != nil {
+			return err
+		}
+	}
 
-    return nil
+	return nil
 }
 
-// downloadFile fungsi untuk mendownload file dari URL tertentu dan menyimpannya ke path lokal.
-func downloadFile(fileURL, savePath string) error {
-    // Buat HTTP request
-    resp, err := http.Get(fileURL)
-    if err != nil {
-        return err
-    }
-    defer resp.Body.Close()
+func downloadFile(fileURL, saveDir string) error {
+	resp, err := http.Get(fileURL)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
-    // Pastikan kita mendapatkan respons HTTP 200 OK.
-    if resp.StatusCode != http.StatusOK {
-        return fmt.Errorf("server return non-200 status: %d %s", resp.StatusCode, resp.Status)
-    }
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("server return non-200 status: %d %s", resp.StatusCode, resp.Status)
+	}
+	fileName := filepath.Base(fileURL)
 
-    // Buat file lokal
-    outFile, err := os.Create(savePath)
-    if err != nil {
-        return err
-    }
-    defer outFile.Close()
+	savePath := filepath.Join(saveDir, fileName)
+	outFile, err := os.Create(savePath)
+	if err != nil {
+		return err
+	}
 
-    // Salin data dari HTTP response ke file lokal
-    _, err = io.Copy(outFile, resp.Body)
-    return err // akan nil jika tidak ada error
+
+	defer outFile.Close()
+	_, err = io.Copy(outFile, resp.Body)
+	return err
+}
+
+func removeFiles(dirPath string) error {
+	dir, err := os.Open(dirPath)
+	if err != nil {
+		return err
+	}
+	defer dir.Close()
+
+	files, err := dir.Readdir(-1)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		err = os.RemoveAll(filepath.Join(dirPath, file.Name()))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
